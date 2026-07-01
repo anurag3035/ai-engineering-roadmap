815d7381455034471bc51b76f73b9caac947b42a
anurag3035 <janu42962@gmail.com>
2026-07-01 11:23:37 +0530

    Reorganize AI Engineering Roadmap into weekly structure

---
+++ week-3/Day-13/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class Student(BaseModel):
+    id: int
+    name: str
+    age: int
+    course: str
+
+
+class StudentCreate(BaseModel):
+    name: str
+    age: int
+    course: str
